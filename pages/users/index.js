7cc54e83831958fbad187a3344ad8f72730207cf
ctdeakin <ctdeakin@gmail.com>
2022-03-20 18:07:54 -0400

    add: nav bar with routes to pages for each slice of dataset

---
+++ pages/users/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+export default function users () {
+    return <div>
+        <Layout />
+    <h2>Users</h2>
+    </div>
+}
