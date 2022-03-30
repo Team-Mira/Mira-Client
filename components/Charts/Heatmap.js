ad9516f7e26c28dbfadc26c4ef7fc866eae378f9
Allan <allanwang97@gmail.com>
2022-03-30 11:55:37 -0400

    heatmaps

---
+++ components/Charts/Heatmap.js
@@ -0,0 +1,22 @@
+import Chart from "react-apexcharts"
+
+
+export default function Heatmap({data}) {
+  const options
+  return (
+    <Chart></Chart>
+  )
+
+}
+// var options = {
+//   chart: {
+//     type: 'line'
+//   },
+//   series: [{
+//     name: 'sales',
+//     data: [30,40,35,50,49,60,70,91,125]
+//   }],
+//   xaxis: {
+//     categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+//   }
+// }
