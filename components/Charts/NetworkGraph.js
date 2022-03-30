4206b38e4e4763de5fcb59a3cfefba74d9bdccbe
Robert Brown <robert.t.brown352@gmail.com>
2022-03-30 11:43:09 -0400

    Added avatars to the network graph

--- components/Charts/NetworkGraph.js
+++ components/Charts/NetworkGraph.js
@@ -1,19 +1,15 @@
 import Graph from 'react-graph-vis'
-import {useState} from 'react'
-import Tooltip from '@mui/material'
 import {Box} from '@mui/system'
 
 function NetworkGraph({data}) {
   let {updateUserGraph} = data
 
-  const[tipFocus, setTipFocus] = useState('')
-
   const options = {
     height: "100%",
     zoomView: false,
     nodes: {
-      shape: "dot",
-      color: 'rgba(54, 162, 235, 1)'
+      shape: "circularImage",
+      borderWidth: 0
     },
     edges: {
       color: "rgba(255, 99, 132, .5",
@@ -34,14 +30,11 @@ function NetworkGraph({data}) {
           enabled: false
         }
       }
-    } 
+    }
   }
 
-  const events = {
-  
-  }
   return (
-    <Box sx={{height: 300, position: 'relative'}}>  
+    <Box sx={{height: 300, position: 'relative'}}>
 
       <Graph graph={updateUserGraph} options={options} />
     </Box>
