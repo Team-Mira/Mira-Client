43f345cc78904204fb48aae9ee24cb19c03b4002
Allan <allanwang97@gmail.com>
2022-04-05 15:55:37 -0400

    merged heat maps

--- components/Charts/Heatmap.js
+++ components/Charts/Heatmap.js
@@ -10,11 +10,19 @@ export default function Heatmap({data}) {
       },
       colors: ['#E91E63'],
       title: {
-        text: 'HeatMap'
+        text: `${data.name} HeatMap`,
+        style: {
+          color: '#FFFFFF'
+        }
       },
       xaxis: {
         categories: ['12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am', '9am', '10am', '11am',
-      '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm']
+      '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm'],
+        labels: {
+            style: {
+              colors: '#FFFFFF'
+            }
+          }
       },
       chart: {
         height: '100%',
@@ -51,7 +59,7 @@ export default function Heatmap({data}) {
 
   const series = channels.map(channel => ({
     name: channel.name,
-    data: channel.activity
+    data: channel.activity,
   }))
 
 
